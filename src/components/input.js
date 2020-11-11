6d65c20a1ea8ab7a3ec7b1846c6d0d0d857a820f
wellingtoncgs30 <wellingtoncgs30@gmail.com>
2020-11-11 14:26:52 -0300

    Corrigindo componente input

--- src/components/input.js
+++ src/components/input.js
@@ -5,7 +5,7 @@ import { Form } from "react-bootstrap"
 export default function Input(props) {
     return(
         <InputMask mask="(99)99999-9999" value={props.value} onChange={props.onChange}>
-            {(inputProps) => <Form.Control {...inputProps} placeholder="Telefone:" required></Form.Control>}
+            {(inputProps) => <Form.Control {...inputProps} name="telephone" placeholder="Telefone:" required></Form.Control>}
         </InputMask>
     )
 }
